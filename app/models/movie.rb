8f598e5f04e274004832ce4c67e7b1c6ce99dd43
Jon foster <redcabinny@gmail.com>
2017-04-05 12:37:50 -0400

    Added movie relationships

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+	has_many :movie_actors
+	has_many :actors, through: :movie_actors
+	has_many :movie_locations
+	has_many :locations, through: :movie_locations
+	has_many :comments
+	belongs_to :category
 end
