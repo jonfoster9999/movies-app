49b6407261b99c64bd6787843074fb8e296e6d3e
Jon foster <redcabinny@gmail.com>
2017-04-09 22:41:32 -0400

    built out update box office feature

--- app/assets/javascripts/ng-app/controllers/MovieCtrl.js
+++ app/assets/javascripts/ng-app/controllers/MovieCtrl.js
@@ -1,4 +1,7 @@
 function MovieCtrl($scope, $http, $stateParams, movie){
+	var selectedId = -1
+	var editFlag = false
+
 	$scope.movie = movie.data
 	$scope.comments = $scope.movie.comments
 	$scope.formData = {}
@@ -7,6 +10,7 @@ function MovieCtrl($scope, $http, $stateParams, movie){
 	$scope.isInEditMode = isInEditMode;
 	$scope.save = save
 	$scope.isInReadMode = isInReadMode;
+	$scope.cancel = cancel;
 	$scope.submitted = function(){
 		$http({
 			method: 'POST',
@@ -21,22 +25,39 @@ function MovieCtrl($scope, $http, $stateParams, movie){
 		})
 	}
 
-
+	function reset() {
+		selectedId = -1;
+		editFlag = false
+	}
 
 	function startEdit(id) {
-
+		selectedId = id;
 	}
 
 	function isInEditMode() {
-		return true;
+		return selectedId >= 0;
 	}
 
 	function save(id) {
-		alert("YOOOOO")
+		var newBoxOffice = $scope.movie.box_office;
+		var id = id;
+		$http({
+			method: 'PATCH',
+			beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+			url: '/movies/' + id,
+			data: {newBoxOffice: newBoxOffice}
+		}).then(function(data){
+			$scope.movie.box_office = data.data.box_office;
+			reset();
+		})
 	}
 
-	function isInReadMode(){
-		return true
+	function isInReadMode(id){
+		return selectedId < 0
+	}
+
+	function cancel(){
+		reset();
 	}
 }
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,13 @@ class MoviesController < ApplicationController
 		render json: Movie.all
 	end
 
+	def update
+		movie = Movie.find(params[:id])
+		movie.box_office = params[:newBoxOffice].to_i
+		movie.save
+		render json: movie
+	end
+
 	def show
 		render json: Movie.find(params[:id])
 	end
